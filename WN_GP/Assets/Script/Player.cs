1f37fcc783376e520f34568606787b4516f7b315
AliyAita <wing80839@gmail.com>
2026-04-14 14:19:58 +0800

    角色移動

--- WN_GP/Assets/Script/Player.cs
+++ WN_GP/Assets/Script/Player.cs
@@ -2,8 +2,36 @@ using UnityEngine;
 
 public class Player: MonoBehaviour
 {
-    [Header("水平移動")]
-    public float speedX;
-    [Header("垂直移動")]
-    public float speedY;
+    Vector3 direction;
+    [Header("移動速度")]
+    public float speed;
+
+   
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            Debug.Log("W");
+            direction = Vector3.forward * (speed * Time.deltaTime);
+            transform.Translate(direction);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            Debug.Log("D");
+            direction = Vector3.right * (speed * Time.deltaTime);
+            transform.Translate(direction);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            Debug.Log("S");
+            direction = Vector3.back * (speed * Time.deltaTime);
+            transform.Translate(direction);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            Debug.Log("A");
+            direction = Vector3.left * (speed * Time.deltaTime);
+            transform.Translate(direction);
+        }
+    }
 }
