47940cc6223fbb68c994bef6b0e149c03d75f4ea
wink44701484 <wink44701484@gmail.com>
2026-04-26 18:38:04 +0800

    接近物品偵測與互動

---
+++ WN_GP/Assets/Script/Bed.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Bed : MonoBehaviour, Interactable
+{
+
+    public void TriggerAction()
+    {
+        Debug.Log("options");
+    }
+
+}
---
+++ WN_GP/Assets/Script/Sign.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class Sign : MonoBehaviour
+{
+    public Transform playerTrans;
+
+    public GameObject signSprite;
+
+    private Interactable targetItem;
+
+    private bool canPress;
+
+
+    private void Update()
+    {
+        signSprite.SetActive(canPress);
+        signSprite.transform.localScale=playerTrans.localScale;
+
+        if (canPress && Input.GetKeyDown(KeyCode.X))
+        {
+            targetItem.TriggerAction();
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        if(collision.gameObject.CompareTag("Interactable"))
+        {
+            canPress = true;
+            targetItem = collision.gameObject.GetComponent<Interactable>();
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+
+        if (collision.gameObject.CompareTag("Interactable"))
+        {
+            canPress = false;
+        }
+    }
+
+
+}
